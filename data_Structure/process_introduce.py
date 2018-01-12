fb546aeb188cfd496103f6061691e1ee478ffa5a
fengyifei <171064795@qq.com>
2018-01-12 14:21:03 +0800

    处理履历，得到时间-职位

--- data_Structure/process_introduce.py
+++ data_Structure/process_introduce.py
@@ -9,17 +9,18 @@ def get_year_pos(text, years):
     year_pos = []
     year_pos.append(may_pos[0])
     for i in range(1, len(may_pos)):
-        if may_pos[i] - may_pos[i-1] > 10:
+        if may_pos[i] - may_pos[i - 1] > 10:
             year_pos.append(may_pos[i])
     return year_pos
 
 
 def get_segments(text, year_pos):
     segments = []
-    for i in range(len(year_pos)-1):
-        segments.append(text[ year_pos[i] : year_pos[i+1] ])
-    segments.append(text[ year_pos[len(year_pos)-1] : ])
+    for i in range(len(year_pos) - 1):
+        segments.append(text[year_pos[i]: year_pos[i + 1]])
+    segments.append(text[year_pos[len(year_pos) - 1]:])
     return segments
+
 
 def process_segment(segment):
     if segment.find('，') >= 0:
@@ -45,14 +46,14 @@ def process_last_segment(segment):
 def get_divide_text(text):
     pos = []
     right_poi = 0
-    while right_poi < len(text)-1:
+    while right_poi < len(text) - 1:
         left_poi = text[right_poi:].find('（')
         if left_poi < 0:
             break
 
-        #找对应右括号
+        # 找对应右括号
         right = 0
-        for i in range(left_poi+1, len(text)):
+        for i in range(left_poi + 1, len(text)):
             if text[i] == '）':
                 if right == 0:
                     right_poi = i
@@ -65,24 +66,21 @@ def get_divide_text(text):
 
     divide_text = []
     for item in pos:
-        divide_text.append(text[ item[0]+1:item[1] ])
+        divide_text.append(text[item[0] + 1:item[1]])
 
     base = 0
     for item in pos:
         left = item[0] + base
-        right = item[1] +base
-        text = text[0:left] + text[right+1:]
+        right = item[1] + base
+        text = text[0:left] + text[right + 1:]
         base = right - left + 1
     divide_text.append(text)
 
     return divide_text
 
 
-
-
-
 def process_introduce(text):
-    divide_text = get_divide_text(text)     #把括号中的内容提出来
+    divide_text = get_divide_text(text)  # 把括号中的内容提出来
 
     segments = []
     for item in divide_text:
@@ -91,9 +89,8 @@ def process_introduce(text):
         part_segments = get_segments(item, year_pos)
         segments += part_segments
 
-
     time_and_work = []
-    for i in range(len(segments)-1):
+    for i in range(len(segments) - 1):
         time, work = process_segment(segments[i])
         time_and_work.append([time, work])
     last_time, last_work, other_info = process_last_segment(segments[-1])
@@ -101,6 +98,7 @@ def process_introduce(text):
     time_and_work.append(other_info)
 
     return time_and_work
+
 
 if __name__ == "__main__":
     text = 'zzzz（kk9090，（）zzz（jjj）ddd）zzzzzzzzz12zz9999bb7777cc，cccccccc'
