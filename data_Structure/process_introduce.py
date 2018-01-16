5087f036b8fbc2204f7fd53ecd7174f107ed9ce8
limenglong <mr.df@qq.com>
2018-01-16 10:50:00 +0800

    将更新脚本和结构化模块规整(添加注释等)v

--- data_Structure/process_introduce.py
+++ data_Structure/process_introduce.py
@@ -1,24 +1,22 @@
-#!user/bin/env python3
-# -*- coding: gbk -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import re
 
 
-def get_year_pos(text):
-
-    may_all_years = re.findall('\d\d\d\d', text)
+def get_year_pos(mess_text):
+    may_all_years = re.findall('\d\d\d\d', mess_text)
     all_years = []
     for year in may_all_years:
-        if int(year) > 1900 and int(year) <= 2018:
+        if 1900 < int(year) <= 2018:
             all_years.append(str(year))
-
 
     if len(all_years) == 0:
         return
 
     may_pos = []
     last_pos = 0
-    for item in all_years:
-        pos = text[last_pos:].index(item) + last_pos
+    for year_item in all_years:
+        pos = mess_text[last_pos:].index(year_item) + last_pos
         may_pos.append(pos)
         last_pos = pos + 1
     year_pos = [may_pos[0]]
@@ -28,59 +26,57 @@ def get_year_pos(text):
     return year_pos
 
 
-def get_segments(text, year_pos):
+def get_segments(mess_text, year_pos):
     segments = []
     for i in range(len(year_pos) - 1):
-        segments.append(text[year_pos[i]: year_pos[i + 1]])
-    segments.append(text[year_pos[len(year_pos) - 1]:])
+        segments.append(mess_text[year_pos[i]: year_pos[i + 1]])
+    segments.append(mess_text[year_pos[len(year_pos) - 1]:])
     return segments
 
 
 def process_segment(segment):
-    if segment.find('£¬') >= 0:
-        seg_split = segment.split('£¬')
+    if segment.find('ï¼Œ') >= 0:
+        seg_split = segment.split('ï¼Œ')
         return seg_split[0], seg_split[1]
     else:
-        if 'ÈÕºó' in segment:
-            mark = segment.index("ÈÕºó")
+        if 'æ—¥å' in segment:
+            mark = segment.index("æ—¥å")
             return segment[:mark], segment[mark + 2:]
-        elif 'ÔÂºó' in segment:
-            mark = segment.index("ÔÂºó")
+        elif 'æœˆå' in segment:
+            mark = segment.index("æœˆå")
             return segment[:mark], segment[mark + 2:]
 
-        elif 'ÈÕ' in segment:
-            mark = len(segment) - segment[::-1].index(re.findall('ÈÕ', segment[::-1])[0])
+        elif 'æ—¥' in segment:
+            mark = len(segment) - segment[::-1].index(re.findall('æ—¥', segment[::-1])[0])
             return segment[:mark], segment[mark:]
-        elif 'ÔÂ' in segment:
-            mark = len(segment) - segment[::-1].index(re.findall('ÔÂ', segment[::-1])[0])
+        elif 'æœˆ' in segment:
+            mark = len(segment) - segment[::-1].index(re.findall('æœˆ', segment[::-1])[0])
             return segment[:mark], segment[mark:]
         else:
             mark = len(segment) - segment[::-1].index(re.findall('\d', segment[::-1])[0])
             return segment[:mark], segment[mark:]
 
-        return segment, ''
-
 
 def process_last_segment(segment):
-    if segment.find('£»') > 0 or segment.find('¡£') > 0:
-        seg_split_pos = segment.find('£»')
+    if segment.find('ï¼›') > 0 or segment.find('ã€‚') > 0:
+        seg_split_pos = segment.find('ï¼›')
         if seg_split_pos <= 0:
-            seg_split_pos = segment.find('¡£')
+            seg_split_pos = segment.find('ã€‚')
         time, work = process_segment(segment[0: seg_split_pos])
-        other_info = segment[seg_split_pos+1:]
+        other_info = segment[seg_split_pos + 1:]
     else:
         time, work = process_segment(segment)
         other_info = ''
     return time, work, other_info
 
 
-def get_divide_text(text):
+def get_divide_text(mess_text):
     pos = []
     right_poi = 0
     left_poi = 0
-    while right_poi < len(text) - 1:
-        english_left_poi = text[right_poi:].find('(') + right_poi
-        chinese_left_poi = text[right_poi:].find('£¨') + right_poi
+    while right_poi < len(mess_text) - 1:
+        english_left_poi = mess_text[right_poi:].find('(') + right_poi
+        chinese_left_poi = mess_text[right_poi:].find('ï¼ˆ') + right_poi
         if english_left_poi < right_poi and chinese_left_poi < right_poi:
             break
         else:
@@ -91,43 +87,43 @@ def get_divide_text(text):
             else:
                 left_poi = chinese_left_poi if chinese_left_poi < english_left_poi else english_left_poi
 
-        # ÕÒ¶ÔÓ¦ÓÒÀ¨ºÅ
+        # æ‰¾å¯¹åº”å³æ‹¬å·
         right = 0
-        for i in range(left_poi + 1, len(text)):
-            if text[i] == '£©' or text[i] == ')':
+        for i in range(left_poi + 1, len(mess_text)):
+            if mess_text[i] == 'ï¼‰' or mess_text[i] == ')':
                 if right == 0:
                     right_poi = i
                     pos.append([left_poi, right_poi])
                     break
                 else:
                     right -= 1
-            if text[i] == '£¨' or text[i] == '(':
+            if mess_text[i] == 'ï¼ˆ' or mess_text[i] == '(':
                 right += 1
         if right_poi <= left_poi:
             right_poi = left_poi + 1
 
     divide_text = []
-    for item in pos:
-        divide_text.append(text[item[0] + 1:item[1]])
+    for pos_item in pos:
+        divide_text.append(mess_text[pos_item[0] + 1:pos_item[1]])
 
     base = 0
-    for item in pos:
-        left = item[0] - base
-        right = item[1] - base
-        text = text[0:left] + text[right + 1:]
+    for pos_item in pos:
+        left = pos_item[0] - base
+        right = pos_item[1] - base
+        mess_text = mess_text[0:left] + mess_text[right + 1:]
         base += right - left + 1
-    divide_text.append(text)
+    divide_text.append(mess_text)
 
     return divide_text
 
 
-def process_introduce(text):
-    divide_text = get_divide_text(text)  # °ÑÀ¨ºÅÖĞµÄÄÚÈİÌá³öÀ´
+def process_introduce(mess_text):
+    divide_text = get_divide_text(mess_text)  # æŠŠæ‹¬å·ä¸­çš„å†…å®¹æå‡ºæ¥
 
     segments = []
     for item in divide_text:
         year_pos = get_year_pos(item)
-        if year_pos == None:
+        if year_pos is None:
             continue
         part_segments = get_segments(item, year_pos)
         segments += part_segments
@@ -140,14 +136,14 @@ def process_introduce(text):
         time_and_work.append([time, work])
     last_time, last_work, other_info = process_last_segment(segments[-1])
     time_and_work.append([last_time, last_work])
-    time_and_work.append([other_info]) if len(other_info) > 0 else None
+    time_and_work.append([other_info]) if len(other_info) > 0 else ''
 
     return time_and_work
 
 
 if __name__ == "__main__":
-    text = '19£´£¹Äê£±ÔÂÎ÷º£¹¤ÉÌ¾ÖË°Îñ¿ÆÉó¼ÆÔ±£»£±£¹£´£¹Äê£±ÔÂ-£±£¹£µ£µÄê£³ÔÂÊ¡²ÆÕşÌüºÍË°Îñ¾Ö°ìÊÂÔ±¡¢¿ÆÔ±£»£±£¹£µ£µÄê£³ÔÂ'
-    struct_data = process_introduce(text)
-    if struct_data != None:
-        for item in struct_data:
+    text = '19ï¼”ï¼™å¹´ï¼‘æœˆè¥¿æµ·å·¥å•†å±€ç¨åŠ¡ç§‘å®¡è®¡å‘˜ï¼›ï¼‘ï¼™ï¼”ï¼™å¹´ï¼‘æœˆ-ï¼‘ï¼™ï¼•ï¼•å¹´ï¼“æœˆçœè´¢æ”¿å…å’Œç¨åŠ¡å±€åŠäº‹å‘˜ã€ç§‘å‘˜ï¼›ï¼‘ï¼™ï¼•ï¼•å¹´ï¼“æœˆ'
+    structural_data = process_introduce(text)
+    if structural_data is not None:
+        for item in structural_data:
             print(item)
